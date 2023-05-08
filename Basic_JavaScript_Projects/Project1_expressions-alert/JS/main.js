de9c83bedbb7cf7737a0193cd042987d079bc629
Joshua Dishong <dishong.joshua@yahoo.com>
2023-05-08 10:39:36 -0400

    added an index and js file

---
+++ Basic_JavaScript_Projects/Project1_expressions-alert/JS/main.js
@@ -0,0 +1,26 @@
+var A = " This is a string.";  //Assigning a string to variable A.
+var B = ' My name is Joshua Dishong' + ' and' + ' I\'m a software developer.'; //Assigning 3 strings added together to variable B.
+var Name = "Josh", Age = "30", Color = "Purple"; //Assigning multiple variables in one statement.
+window.alert("Hello, World!") //Adding an alert box with a string.
+
+window.alert("Hey you, " + A + "!"); //Adding an alert box to include a variable
+
+//FUNCTION
+function My_First_Function() {            //Defining a function and naming it.
+    var str = "This is the button text!"; //Defining a variable and givig it a string value.
+
+    document.getElementById("Button_Text")//Putting the value of the variable ino the HTML elementFromPoint
+                                          //with the "Button=Text" id.
+}
+
+
+
+document.write("Hello, World!");
+
+document.write(A);
+
+document.write(B);
+
+document.write(Color);
+
+document.write(3 + 3); //Adding an expression
